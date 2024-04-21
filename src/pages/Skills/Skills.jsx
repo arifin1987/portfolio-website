346e644848c9c5091f9ff0fdc850fd4f36d02d44
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-04-21 09:40:24 -0700

    portfolio updated

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,44 +1,71 @@
-import { FaReact,FaNodeJs, FaBootstrap,} from 'react-icons/fa';
-import { BiLogoTailwindCss,BiLogoFirebase,BiLogoMongodb } from "react-icons/bi";
-import { SiExpress } from "react-icons/si";
-import { Flip } from 'react-awesome-reveal';
-
+import { FaReact, FaNodeJs, FaBootstrap } from "react-icons/fa";
+import {
+  BiLogoTailwindCss,
+  BiLogoFirebase,
+  BiLogoMongodb,
+} from "react-icons/bi";
+import { SiExpress, SiRedux, SiTypescript } from "react-icons/si";
+import { Flip } from "react-awesome-reveal";
 
 const Skills = () => {
-    return (
-       
-        <div>
-            <Flip>
-            <h1 className="text-center text-4xl m-6">Skills</h1>
+  return (
+    <div>
+      <Flip>
+        <h1 className="text-center text-4xl m-6">Skills</h1>
+      </Flip>
 
-            </Flip>
-             
-             <div className='md:flex justify-between'>
-                <div>
-                    <h1 className='text-2xl text-cyan-500'> <FaReact></FaReact>React.js</h1>
-                </div>
-                <div>
-                    <h1 className='text-2xl text-emerald-500'><FaNodeJs></FaNodeJs>Node.js</h1>
-                </div>
-                <div>
-                    <h1 className='text-2xl text-emerald-700'><BiLogoMongodb></BiLogoMongodb>Mongo DB</h1>
-                </div>
-                <div>
-                    <h1 className='text-2xl text-black'><SiExpress></SiExpress> Express.js</h1>
-                </div>
-                <div>
-                    <h1 className='text-2xl text-cyan-500'><BiLogoTailwindCss></BiLogoTailwindCss>Tailwind Css </h1>
-                </div>
-                <div>
-                    <h1 className='text-2xl text-sky-600'><FaBootstrap></FaBootstrap>Bootstrap</h1>
-                </div>
-                <div>
-                    <h1 className='text-2xl text-yellow-500'><BiLogoFirebase></BiLogoFirebase>Firebase</h1>
-                </div>
-             </div>
-            
+      <div className="md:flex justify-between">
+        <div>
+          <h1 className="text-2xl text-cyan-500">
+            {" "}
+            <SiTypescript></SiTypescript>TypeScript
+          </h1>
         </div>
-    );
+        <div>
+          <h1 className="text-2xl text-cyan-500">
+            {" "}
+            <FaReact></FaReact>React.js
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl text-cyan-500">
+            {" "}
+            <SiRedux></SiRedux>Redux
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl text-emerald-500">
+            <FaNodeJs></FaNodeJs>Node.js
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl text-emerald-700">
+            <BiLogoMongodb></BiLogoMongodb>Mongo DB
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl text-black">
+            <SiExpress></SiExpress> Express.js
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl text-cyan-500">
+            <BiLogoTailwindCss></BiLogoTailwindCss>Tailwind Css{" "}
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl text-sky-600">
+            <FaBootstrap></FaBootstrap>Bootstrap
+          </h1>
+        </div>
+        <div>
+          <h1 className="text-2xl text-yellow-500">
+            <BiLogoFirebase></BiLogoFirebase>Firebase
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
